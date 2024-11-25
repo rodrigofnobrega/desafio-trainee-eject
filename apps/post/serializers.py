00a7588e74fe502c9fdca4767896d98c82563112
Rodrigo <rofen1@protonmail.com>
2024-11-25 20:45:07 -0300

    feat: Adicionar campo id e aninhar relações no serializer

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import PostModel
+from apps.user.serializers import UserSerializer
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
     class Meta:
         model = PostModel
-        fields = ['user', 'title', 'content']
+        fields = (
+            'id',
+            'title',
+            'content',
+            'publication_date',
+            'user',
+        )
