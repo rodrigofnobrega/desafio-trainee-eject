d5a5431a5dc9bd053f16df110ba6d36c44cceb7d
Rodrigo <rofen1@protonmail.com>
2024-11-25 21:14:57 -0300

    feat: Obter todos os posts de um usuário

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import PostModel
 from .serializers import PostSerializer
 
@@ -6,3 +8,12 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = PostModel.objects.all()
     serializer_class = PostSerializer
+
+    # Endpoint para obter todos os posts de um usuário pelo ID do usuário.
+    @action(detail=False, methods=['get'], url_path='user/(?P<user_id>[^/.]+)')
+    def posts_by_user(self, request, user_id=None):
+        print(user_id)
+        posts = PostModel.objects.filter(user_id=user_id)  
+        serializer = self.get_serializer(posts, many=True)
+
+        return Response(serializer.data)
