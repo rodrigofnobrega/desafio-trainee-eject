8c40edfb19e6493c1236eac6f897355a809c9646
Rodrigo <rofen1@protonmail.com>
2024-11-27 22:01:18 -0300

    feat: Atualizar post serializer para retornar objetos usuário e post

--- apps/post_comments/serializers.py
+++ apps/post_comments/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
 from .models import CommentModel
+from apps.user.models import UserModel
+from apps.user.serializers import UserSerializer
+from apps.post.models import PostModel
+from apps.post.serializers import PostSerializer
 
 class CommentSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(queryset=PostModel.objects.all())
+    user = serializers.PrimaryKeyRelatedField(queryset=UserModel.objects.all())
+
     class Meta:
         model = CommentModel
-        fields = ['content', 'user', 'post']
+        fields = (
+            'id',
+            'content',
+            'created_at',
+            'updated',
+            'user',
+            'post'
+        )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['user'] = UserSerializer(instance.user).data
+        representation['post'] = PostSerializer(instance.post).data
+
+        return representation
