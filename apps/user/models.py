27e836ee0cbd18b3bb91ab8784c05b8c23f49d35
Rodrigo <rofen1@protonmail.com>
2024-11-27 22:34:23 -0300

    feat: Criar métodos to string para os models

--- apps/user/models.py
+++ apps/user/models.py
@@ -8,3 +8,6 @@ class UserModel(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} | {self.email}"
